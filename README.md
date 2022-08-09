b8f169237c665c21e0b7a7282ff55085bb2461f7
@Draichi <lucasdraichi@gmail.com>
2022-08-08 22:56:23 -0300

    feat: creates galaxyGenerator function

---
+++ README.md
@@ -0,0 +1,13 @@
+# Galaxy Generator
+
+3D Galaxy generator study case
+
+## Setup
+
+```sh
+# development
+yarn && yarn dev
+
+# build for production
+yarn build
+```
---
+++ src/main.ts
@@ -0,0 +1,109 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import GUI from "lil-gui";
+
+interface GalaxyGenerator {
+  numberOfParticles: number;
+  size: number;
+}
+
+const gui = new GUI();
+
+const canvas = document.querySelector("canvas#webgl") as HTMLElement;
+
+const scene = new THREE.Scene();
+
+let material: THREE.PointsMaterial;
+let geometry: THREE.BufferGeometry;
+let points: THREE.Points;
+
+const galaxyGenerator = ({ numberOfParticles, size }: GalaxyGenerator) => {
+  if (points) {
+    material.dispose();
+    geometry.dispose();
+    scene.remove(points);
+  }
+  geometry = new THREE.BufferGeometry();
+  const numberOfDimensions = 3; // x, y, z
+  const positions = new Float32Array(numberOfParticles * numberOfDimensions);
+
+  for (let i = 0; i < numberOfParticles; i++) {
+    const x = i * numberOfDimensions;
+    const y = x + 1;
+    const z = x + 2;
+
+    positions[x] = (Math.random() - 0.5) * 10;
+    positions[y] = (Math.random() - 0.5) * 10;
+    positions[z] = (Math.random() - 0.5) * 10;
+  }
+
+  geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+
+  material = new THREE.PointsMaterial({
+    size,
+    sizeAttenuation: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+  });
+
+  points = new THREE.Points(geometry, material);
+  scene.add(points);
+};
+const galaxyParams: GalaxyGenerator = {
+  numberOfParticles: 1000,
+  size: 0.01,
+};
+galaxyGenerator(galaxyParams);
+
+gui
+  .add(galaxyParams, "numberOfParticles")
+  .min(100)
+  .max(1000000)
+  .step(100)
+  .onFinishChange(() => galaxyGenerator(galaxyParams));
+gui
+  .add(galaxyParams, "size")
+  .min(0.001)
+  .max(0.1)
+  .step(0.001)
+  .onFinishChange(() => galaxyGenerator(galaxyParams));
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  1,
+  100
+);
+camera.position.set(3, 5, 10);
+scene.add(camera);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+const tick = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+tick();
